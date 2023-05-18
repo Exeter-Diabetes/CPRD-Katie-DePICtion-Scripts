ce6020658c5a38f29ee3a0f84cf5a5dcc10fcf1b
drkgyoung <k.young3@exeter.ac.uk>
2023-05-18 10:37:08 +0100

    Update 03_dpctn_diabetes_qof_primis_codelist.R

--- 03_dpctn_diabetes_qof_primis_codelist.R
+++ 03_dpctn_diabetes_qof_primis_codelist.R
@@ -17,8 +17,10 @@ analysis = cprd$analysis("dpctn")
 
 ############################################################################################
 
-# Get cohort info with classes
+# Set index date
+index_date <- as.Date("2020-02-01")
 
+# Get cohort info with classes
 cohort_classification <- cohort_classification %>% analysis$cached("cohort_classification")
 
 ############################################################################################
@@ -33,17 +35,25 @@ raw_qof_diabetes_medcodes <- cprd$tables$observation %>%
 
 analysis = cprd$analysis("dpctn")
 
-with_qof <- raw_qof_diabetes_medcodes %>%
+latest_qof <- raw_qof_diabetes_medcodes %>%
   inner_join(cprd$tables$validDateLookup, by="patid") %>%
-  filter(obsdate>=min_dob & obsdate<=gp_ons_end_date) %>%
-  distinct(patid)
+  filter(obsdate>=min_dob & obsdate<=index_date) %>%
+  group_by(patid) %>%
+  summarise(latest_qof=max(obsdate, na.rm=TRUE)) %>%
+  ungroup()
 
 cohort_with_qof <- cohort_classification %>%
   select(patid, class) %>%
-  inner_join(with_qof, by="patid") %>%
+  inner_join(latest_qof, by="patid") %>%
   analysis$cached("cohort_with_qof", unique_indexes="patid", indexes="class")
 
 counts <- collect(cohort_with_qof %>% group_by(class) %>% count())
+
+time_to_latest <- collect(cohort_with_qof %>%
+  mutate(datediff_var=datediff(index_date, latest_qof))) %>%
+  group_by(class) %>%
+  summarise(median_time=median(datediff_var, na.rm=TRUE)) %>%
+  ungroup()
 
 
 ############################################################################################
@@ -56,29 +66,68 @@ primis <- read_csv("primis-covid19-vacc-uptake-diab-v.1.5.3.csv", col_types=cols
 #545
 
 ## Merge with aurum Medical Dictionary
-setwd("C:/Users/ky279/OneDrive - University of Exeter/CPRD/New dictionaries")
-aurum <- read_delim("CPRDAurumMedical.txt", col_types=cols(.default=col_character()))
+### Using old version: more recent versions contain more codes but no more diabetes codes are used in our dataset
+setwd("C:/Users/ky279/OneDrive - University of Exeter/CPRD/CPRD_Aurum/Codelists and algorithms/Reference files/202005_Lookups_CPRDAurum")
+aurum <- read_delim("202005_EMISMedicalDictionary.txt", col_types=cols(.default=col_character()))
 
 
 ## Find which occur in medical dictionary
 primis_aurum <- primis %>% inner_join(aurum, by=c("code"="SnomedCTConceptId"))
-#1,415
+#753
 
 primis_aurum %>% distinct(code) %>% count()
-#458
+#187
 
 
 ## How many in the list that defines our cohort
 
-dpctn_diabetes_list <- collect(codes$all_diabetes %>% union(codes$exclusion_diabetes))
+dpctn_diabetes_list <- collect(codes$all_diabetes %>% union((codes$exclusion_diabetes %>% filter(exclusion_diabetes_cat !="diabetes_insipidus"))))
+#1,361
+
+primis_aurum %>% inner_join(dpctn_diabetes_list, by=c("MedCodeId"="medcodeid")) %>% count()
+#711
+
+primis_aurum %>% anti_join(dpctn_diabetes_list, by=c("MedCodeId"="medcodeid")) %>% count()
+#42
+
+primis_extras <- primis_aurum %>% anti_join(dpctn_diabetes_list, by=c("MedCodeId"="medcodeid"))
+# Most are infrequently used codes with ^ESCT Read codes, but also includes (some include diabetes mellitus in description in their codelist - 3, 11-16):
+
+# 1 O/E - right eye clinically significant macular oedema 2BBm            
+# 2 O/E - left eye clinically significant macular oedema  2BBn            
+# 3 Loss of hypoglycaemic warning                         66AJ2           
+# 4 Hypoglycaemic warning absent                          66AJ4           
+# 5 Insulin autoimmune syndrome                           C10J            
+# 6 Insulin autoimmune syndrome without complication      C10J0           
+# 7 Achard - Thiers syndrome                              C152-1          
+# 8 Leprechaunism                                         C1zy3           
+# 9 Donohue's syndrome                                    C1zy3-1         
+# 10 Mauriac's syndrome                                    EMISNQMA111     
+# 11 Ballinger-Wallace syndrome                            ESCTDI21-1      
+# 12 HHS - Hyperosmolar hyperglycaemic syndrome            ESCTDI23-1      
+# 13 HHS - Hyperosmolar hyperglycemic syndrome             ESCTDI23-2      
+# 14 Rogers syndrome                                       ESCTME15-1      
+# 15 Herrmann syndrome                                     ESCTPH1-1       
+# 16 Kimmelstiel - Wilson disease                          K01x1-1
+
+# Can't really see if using these codes changes who is identified as wouldn't have included them in download
+
+# But can see who is our cohort has them
+
+primis_aurum <- primis_aurum %>% select(medcodeid=MedCodeId)
+
+with_primis_clean <- cprd$tables$observation %>%
+  inner_join(primis_aurum, by="medcodeid", copy=TRUE) %>%
+  inner_join(cprd$tables$validDateLookup, by="patid") %>%
+  filter(obsdate>=min_dob & obsdate<=index_date) %>%
+  distinct(patid) %>%
+  analysis$cached("with_primis_clean", indexes="patid")
+
+cohort_with_primis_clean <- cohort_classification %>%
+  select(patid, class) %>%
+  inner_join(with_primis_clean, by="patid") %>%
+  analysis$cached("with_primis_clean", unique_indexes="patid", indexes="class")
 
 
-
-
-primis %>% anti_join(diabetes_snomed, by=c("code"="SnomedCTConceptId"))
-#364
-
-diabetes_snomed %>% anti_join(primis, by=c("SnomedCTConceptId"="code"))
-#450
 
 
